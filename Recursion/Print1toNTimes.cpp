13f2e2cf76337394b2c8537b4bbb379595b2410a
AdityaGoutam18 <gautamaditya182002@gmail.com>
2024-04-29 12:22:35 +0530

    Print linearly 1 to N

--- Recursion/Print1toNTimes.cpp
+++ Recursion/Print1toNTimes.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
- int i=0;  // global variable
-void print(int count){
+//  int i=0;  // global variable
+void print(int i,int count){
    
-    if(i==count) return;
+    if(i>count) return;
     cout<<i<<" ";
-    i++;
-    print(count);
+    print(i+1,count);
 }
 
 
 int main(){
     int count;
     cin>>count;
-    print(count);
+    print(1,count);
     return 0;
 }
