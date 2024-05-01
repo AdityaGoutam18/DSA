bbdb776e1f1075980e6ddfaff849e39304148b49
AdityaGoutam18 <gautamaditya182002@gmail.com>
2024-05-01 07:43:04 +0530

    is Palindrome or not

--- Recursion/Palindrome.cpp
+++ Recursion/Palindrome.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-bool print(int i, string &s){
+bool isPalindrome(int i, string &s){
     if(i>=s.size()/2) return true;
     if(s[i]!=s[s.size()-i-1]) return false;
-    return print(i+1,s);
+    return isPalindrome(i+1,s);
 }
 
 
 
 int main(){
     string s="MADAM";
-    cout<<print(0,s);
+    cout<<isPalindrome(0,s);
 }
