8cd4e822eddcfe34f8877f2c59850c912b861745
AdityaGoutam18 <gautamaditya182002@gmail.com>
2024-05-02 16:53:03 +0530

    max element in an array

--- Arrays/LargestElement.cpp
+++ Arrays/LargestElement.cpp
@@ -6,8 +6,18 @@ int main(){
     // Brute Force
     int arr[5]={3,1,6,12,9};
     int n=5;
-    sort(arr,arr+n);
+    sort(arr,arr+n);          // Time Complexity --> O(NlogN)
     cout<<arr[n-1]<<endl;
+    
+    // Better 
+    int largest=arr[0];
+    for(int i=0;i<n;i++){          // Time Complexity ---> O(N)
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    cout<<largest;
+
     return 0;
     
 }
